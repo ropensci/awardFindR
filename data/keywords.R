daae7dfbcad371ad1f8a61390cc9e34406f54d3f
mccallc <26819445+mccallc@users.noreply.github.com>
2021-02-22 11:39:50 -0500

    CI check other OS/R versions

--- data/keywords.R
+++
@@ -1,8 +0,0 @@
-#' awardFindR keywords list
-#'
-#' For easier reproduction, this file holds a list of keywords
-#' to query in a search. Instead of referencing this file,
-#' these can be specified manually as a vector to awardFindR() as well.
-#'
-#' @format A text file with one string per line
-"keywords"
