3401999b019e4b9a26d673846b1ec65cbdec3dac
mccallc <26819445+mccallc@users.noreply.github.com>
2022-10-17 16:53:09 -0400

    macarthur use json

--- R/macarthur.R
+++ R/macarthur.R
@@ -1,84 +1,52 @@
 #' Search MacArthur foundation for awards
-#' @inheritParams get_nih
+#' @inheritParams get_neh
 #' @return a data.frame
+#' @importFrom jsonlite fromJSON
 #' @export
 #' @examples
 #' macarthur <- get_macarthur("qualitative",
 #' "1999-01-01", "2020-01-01")
-get_macarthur <- function(keyword, from_date, to_date, verbose=FALSE) {
+get_macarthur <- function(keyword, from_year, to_year, verbose=FALSE) {
   url <- "https://searchg2.crownpeak.net/live-macfound-rt/select?"
   parameters <- paste0("q=", xml2::url_escape(keyword),
-                       "&wt=xml&start=0&rows=25494")
+                       "&wt=json&start=0&rows=25494")
 
   # I really don't know what most of this does below, but it was part of the
   # web interface's API query, so I'm leaving it in for now.
   extra <- paste0("&echoParams=explicit&fl=%2A&defType=edismax",
   "&fq=custom_s_template%3A%22grant%20detail%22&sort=score%20desc",
-  "&qf=custom_t_title%20custom_t_description%20custom_t_name",
-  "&indent=true&json.wrf=searchg2_5445608496089546")
+  "&qf=custom_t_title%20custom_t_description%20custom_t_name")
+  #"&indent=true&json.wrf=searchg2_5445608496089546")
 
   query_url <- paste0(url, parameters, extra)
   response <- request(query_url, "get", verbose)
+  response <- jsonlite::fromJSON(response)
 
-  if (xml2::xml_integer(
-    xml2::xml_find_first(response, "/response/result/@numFound"))==0) {
-    return(NULL) # No results?
+  if (response$response$numFound==0) {
+    return(NULL)
   }
 
-  results <- xml2::xml_children(xml2::xml_children(response)[2])
-  results <- lapply(results, function(x) {
-    id <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='id']"))
+  results <- response$response$docs
 
-    institution <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_name']"))
-
-    title <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_title']"))
-
-    description <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_description']"))
-
-    program <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_program_area_code']"))
-
-    amount <- xml2::xml_integer(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_amount']"))
-
-    approved <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_grant_approved_date']"))
-
-    start <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_start_date']"))
-
-    end <- xml2::xml_text(
-      xml2::xml_find_first(x, ".//str[@name='custom_s_end_date']"))
-
-    data.frame(id, institution, approved, start, end, amount, program, title,
-               description, stringsAsFactors = FALSE)
-  })
-  df <- do.call(rbind.data.frame, results)
-
-  #scrap the exact time and format as date
-  df$approved <- substr(df$approved, 1, 10)
-  df$start <- substr(df$start, 1, 10)
-  df$end <- substr(df$end, 1, 10)
-
-  approved <- NULL # For R CMD check
   # Date limit
-  df <- subset(df, as.Date(approved) > from_date & as.Date(approved) < to_date)
-  if (nrow(df)==0) {
+  custom_i_year_approved <- NULL # R CMD Check note issue
+  results <- subset(results, custom_i_year_approved > as.integer(from_year) &
+                      custom_i_year_approved < as.integer(to_year))
+  if (nrow(results)==0) {
     return(NULL) # No results after date limiting?
   }
 
-  df$year <- format.Date(df$approved, "%Y")
-  df$keyword <- keyword
-
-  df
+  results$keyword <- keyword
+  results[, c("id", "custom_s_name", "custom_i_year_approved",
+              "custom_s_start_date", "custom_s_end_date",
+              "custom_s_program_area_code", "custom_s_amount",
+              "custom_s_title", "custom_s_description", "keyword")]
 }
 
 .standardize_macarthur <- function(keywords, from_date, to_date, verbose) {
-  raw <- lapply(keywords, get_macarthur, from_date, to_date, verbose)
+  raw <- lapply(keywords, get_macarthur,
+                format.Date(from_date, "%Y"), format.Date(to_date, "%Y"),
+                verbose)
   raw <- do.call(rbind.data.frame, raw)
   if (nrow(raw)==0) {
     message("No results from MacArthur")
@@ -86,9 +54,12 @@ get_macarthur <- function(keyword, from_date, to_date, verbose=FALSE) {
   }
 
   with(raw, data.frame(
-    institution, pi=NA, year=format.Date(approved, "%Y"),
-    start=substr(start, 1, 10), end=substr(end, 1, 10),
-    program, amount, id, title, abstract=description, keyword,
-    source="MacArthur", stringsAsFactors = FALSE
+    institution=custom_s_name, pi=NA, year=custom_i_year_approved,
+    start=substr(custom_s_start_date, 1, 10),
+    end=substr(custom_s_end_date, 1, 10),
+    program=custom_s_program_area_code,
+    amount=as.integer(custom_s_amount), id, title=custom_s_title,
+    abstract=custom_s_description,
+    keyword, source="MacArthur", stringsAsFactors = FALSE
   ))
 }
--- tests/testthat/test-macarthur.R
+++ tests/testthat/test-macarthur.R
@@ -2,8 +2,8 @@ test_that("MacArthur returns expected result", {
   skip_on_cran()
   suppressMessages(vcr::use_cassette("macarthur", serialize_with="json", {
     macarthur <-
-      .standardize_macarthur("qualitative", "2018-01-01", "2019-01-01",
+      .standardize_macarthur("qualitative", "2015-01-01", "2019-01-01",
                              FALSE)
   }))
-  expect_equal(nrow(macarthur), 1)
+  expect_equal(nrow(macarthur), 3)
 })
