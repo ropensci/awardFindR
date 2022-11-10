ac2b57a27ca83aff58ef1c243eeabfbe4db2fe88
mccallc <26819445+mccallc@users.noreply.github.com>
2022-11-10 15:55:42 -0500

    fixing ophil

--- R/ophil.R
+++ R/ophil.R
@@ -6,38 +6,37 @@
 get_ophil <- function(keyword, from_year, to_year, verbose=FALSE) {
   base_url <- "https://www.openphilanthropy.org/grants?"
   query_url <- paste0(base_url,
-                      "q=\"", xml2::url_escape(keyword), "\"&yr=",
+                      "q=", xml2::url_escape(keyword), "&yr=",
                       paste(from_year:to_year, collapse="&yr="))
 
   response <- request(query_url, "get", verbose)
 
-  title <- response %>% rvest::html_elements(".block-feed-post__title > a") %>%
-    rvest::html_text()
+  # each one of these divs is a result
+  blocks <- response %>% rvest::html_elements("div.block-feed-post")
 
-  if (length(title)==0) {
+  # check for no results
+  if (length(blocks)==0) {
     return(NULL)
   }
+  # loop through each award and extract relevant info
+  awards <- lapply(blocks, function(x) {
+    title <- x %>% rvest::html_element("div.block-feed-post__body > .block-feed-post__title > a") %>% rvest::html_text()
+    date <- x %>% rvest::html_element("div.block-feed-post__body > .block-feed-post__date") %>% rvest::html_text()
+    year <- unlist(lapply(strsplit(date, " "), function(x) { x[2] }))
+    prog <- x %>% rvest::html_element("div.block-feed-post__body > .block-feed-post__focus-area > a") %>% rvest::html_text()
+    org <- x %>% rvest::html_element("div.block-feed-post__body > .block-feed-post__organization-name > a") %>% rvest::html_text()
+    amount <- x %>% rvest::html_element("div.block-feed-post__body > .block-feed-post__grant-amount") %>% rvest::html_text()
+    link <- x %>% rvest::html_element("div.block-feed-post__body > .block-feed-post__link > a") %>% rvest::html_attr("href")
 
-  dates <- response %>% rvest::html_elements(".block-feed-post__date") %>%
-    rvest::html_text2()
 
-  year <- unlist(lapply(strsplit(dates, " "), function(x) { x[2] }))
+    data.frame(organization=org, title, program=prog, amount, year, link)
+  }); awards <- do.call(rbind.data.frame, awards)
+  awards$keyword <- keyword
 
-  organization <- response %>% rvest::html_elements(".block-feed-post__organization-name > a") %>%
-    rvest::html_text()
+  awards$amount <- gsub('\\$', '', awards$amount)
+  awards$amount <- gsub(',', '', awards$amount)
 
-  program <- response %>% rvest::html_elements(".block-feed-post__focus-area > a") %>%
-    rvest::html_text()
-
-  amount <- response %>% rvest::html_elements(".block-feed-post__grant-amount") %>%
-    rvest::html_text()
-  amount <- gsub('\\$', '', amount)
-  amount <- gsub(',', '', amount)
-
-  link <- response %>% rvest::html_elements(".block-feed-post__link > a") %>%
-    rvest::html_attr("href")
-
-  data.frame(organization, title, program, amount, year, link, keyword)
+  awards
 }
 
 .standardize_ophil <- function(keywords, from_date, to_date, verbose) {
