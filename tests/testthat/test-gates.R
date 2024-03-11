32c877b009929e62890edf7cb7ba8dee7b113f14
isaac <isaac.slaughter@gmail.com>
2024-03-11 12:42:57 -0700

    Update gates tests to only record new interactions

--- tests/testthat/test-gates.R
+++ tests/testthat/test-gates.R
@@ -1,6 +1,6 @@
 test_that("Gates foundation search", {
   skip_on_cran()
-  suppressMessages(vcr::use_cassette("gates", serialize_with="json", {
+  suppressMessages(vcr::use_cassette("gates", serialize_with="json", record="new_episodes", {
     gates <- .standardize_gates("qualitative", "2018-01-01", "2019-01-01",
                                 FALSE)
   }))
