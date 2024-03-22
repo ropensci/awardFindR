9de861def06378f7ca8506ce81f9c57a76a6b9aa
isaac <isaac.slaughter@gmail.com>
2024-03-22 14:12:44 -0700

    Add looping to carnegie

--- R/carnegie.R
+++ R/carnegie.R
@@ -8,7 +8,7 @@
 #' }
 get_carnegie <- function(keyword, from_year, to_year, verbose=FALSE) {
   base_url <- "https://www.carnegie.org/grants/grants-database/"
-  query <- paste0("?q=", xml2::url_escape(keyword), "&per_page=104")
+  query <- paste0("?q=", xml2::url_escape(keyword), "&per_page=100")
   url <- paste0(base_url, query)
   for (n in from_year:to_year) url <- paste0(url, "&y=", n)
 
@@ -21,59 +21,94 @@ get_carnegie <- function(keyword, from_year, to_year, verbose=FALSE) {
 
   cookie <- httr::cookies(response)$value # Need this cookie for CSRF validation
   response <- httr::content(response)
-  awards <- rvest::html_nodes(response, "tbody > tr")
-  if (length(awards)==0)  {
+  num_awards <- length(rvest::html_nodes(response, "tbody > tr"))
+  if (num_awards == 0)  {
     return(NULL) # No awards in the table?
   }
 
-  awards <- lapply(awards, function(x) {
-    id <- gsub("^grant-", "", rvest::html_attr(x, "id"))
-    info <- rvest::html_nodes(x, "td") %>% rvest::html_text()
-    # Remove $ and , in amounts (i.e. $1,000,000)
-    amount <- as.integer(gsub("^\\$|,", "", info[3]))
 
-    # Extra details require another HTTP request (per individual award, ugh)
-    # Also, Carnegie now uses CSRF tokens, so we have to include those headers
-    url <- paste0(base_url, "grant/", id, "/")
-    if(verbose==TRUE) message(paste("GET", url, "... "), appendLF=FALSE)
-    response <- httr::GET(url, config=httr::add_headers(
-                      Referer=paste0(base_url,
-                                     "/grantee/knowledgeworks-foundation/"),
-                      `X-CSRFToken`=cookie,
-                      `X-Requested-With`="XMLHttpRequest"),
-                    httr::set_cookies(csfrtoken=cookie), httr::accept_json())
+  more_to_scrape <- TRUE
+  page <- 1
+
+  # Create vectore to store awards in, which we'll iteratively add each page's results to
+  all_awards <- c()
+
+  while (more_to_scrape) {
+    # Create url for this page of results
+    page_url <- paste0(url, "&page=", page)
+
+    # Get new page
+    response <- httr::GET(page_url)
+
+    # Raises statuses, if verbose
     if (verbose==TRUE) {
       httr::message_for_status(response)
       message()
     }
 
-    details <- httr::content(response)
-    details <- xml2::read_html(details$result[1])
+    # Get response
+    response <- httr::content(response)
 
-    # Turn the div table into a real table
-    details <- data.frame(
-      name=rvest::html_text(
-        rvest::html_elements(details, "label.grant-detail--label")), #  ".//label[@class='grant-detail--label']")),
+    # Get awards for new page
+    awards <- rvest::html_nodes(response, "tbody > tr")
 
-      value=rvest::html_text(
-        rvest::html_elements(details, "div.grant-detail--text")), # .//div[@class='grant-detail--text']")),
+    awards <- lapply(awards, function(x) {
+      id <- gsub("^grant-", "", rvest::html_attr(x, "id"))
+      info <- rvest::html_nodes(x, "td") %>% rvest::html_text()
+      # Remove $ and , in amounts (i.e. $1,000,000)
+      amount <- as.integer(gsub("^\\$|,", "", info[3]))
 
-      stringsAsFactors = FALSE)
+      # Extra details require another HTTP request (per individual award, ugh)
+      # Also, Carnegie now uses CSRF tokens, so we have to include those headers
+      url <- paste0(base_url, "grant/", id, "/")
+      if(verbose==TRUE) message(paste("GET", url, "... "), appendLF=FALSE)
+      response <- httr::GET(url, config=httr::add_headers(
+                        Referer=paste0(base_url,
+                                       "/grantee/knowledgeworks-foundation/"),
+                        `X-CSRFToken`=cookie,
+                        `X-Requested-With`="XMLHttpRequest"),
+                      httr::set_cookies(csfrtoken=cookie), httr::accept_json())
+      if (verbose==TRUE) {
+        httr::message_for_status(response)
+        message()
+      }
 
-    # Pull the data we're looking for
-    title <- details$value[details$name=="Project Title"]
-    date <- details$value[details$name=="Date"]
+      details <- httr::content(response)
+      details <- xml2::read_html(details$result[1])
 
-    description <- details$value[details$name=="Description"]
-    if (length(description)==0) description <- NA
+      # Turn the div table into a real table
+      details <- data.frame(
+        name=rvest::html_text(
+          rvest::html_elements(details, "label.grant-detail--label")), #  ".//label[@class='grant-detail--label']")),
 
-    # Merge the data from the search page and the specific result page
-    data.frame(grantee=info[2], date, amount, program=info[4], id, title,
-               year=info[1], abstract=description,
-               keyword, stringsAsFactors = FALSE)
-  })
+        value=rvest::html_text(
+          rvest::html_elements(details, "div.grant-detail--text")), # .//div[@class='grant-detail--text']")),
 
-  do.call(rbind.data.frame, awards)
+        stringsAsFactors = FALSE)
+
+      # Pull the data we're looking for
+      title <- details$value[details$name=="Project Title"]
+      date <- details$value[details$name=="Date"]
+
+      description <- details$value[details$name=="Description"]
+      if (length(description)==0) description <- NA
+
+      # Merge the data from the search page and the specific result page
+      data.frame(grantee=info[2], date, amount, program=info[4], id, title,
+                 year=info[1], abstract=description,
+                 keyword, stringsAsFactors = FALSE)
+    })
+
+    all_awards <- append(all_awards, awards)
+
+    # Check if there is a "next" button on the page, indicating that there's an
+    # an additional page of results to visit
+    more_to_scrape <- length(rvest::html_nodes(response, xpath = "//text()[contains(., 'Next')]/..")) > 0
+    page <- page + 1
+
+  }
+
+  do.call(rbind.data.frame, all_awards)
 }
 
 .standardize_carnegie <- function(keywords, from_date, to_date, verbose) {
--- tests/testthat/test-loop.R
+++ tests/testthat/test-loop.R
@@ -3,7 +3,9 @@ test_that("NIH and NSF multiple page query", {
   suppressMessages(vcr::use_cassette("loop", serialize_with="json", record="new_episodes", {
     nih <- awardFindR::get_nih("dataset", "2019-01-01", "2020-01-01")
     nsf <- awardFindR::get_nsf("qualitative", "2019-01-01", "2019-02-25")
+    carnegie <- awardFindR::get_carnegie("analysis", "2000", "2024")
   }))
   expect_gt(nrow(nih), 500)
   expect_gt(nrow(nsf), 25)
+  expect_gt(nrow(carnegie), 100)
 })
